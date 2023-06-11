012a924bc58ff2b17468b44818b8a98fe25c3833
dr460n4ir3 <token.b@gmail.com>
2023-06-10 22:40:54 -0400

    added notes for what to fix next

--- Services/FighterService/IFighterService.cs
+++ Services/FighterService/IFighterService.cs
@@ -8,6 +8,7 @@ namespace fightAPI.Services.FighterService
     public interface IFighterService
     {
         Task<ServiceResponse<List<GetFighterResponseDto>>> GetAllFighters(int userId);
+        // **** Note: Need to add method to get all fighters for all users ****
         Task<ServiceResponse<GetFighterResponseDto>> GetFighterById(int id);
         Task<ServiceResponse<List<GetFighterResponseDto>>> CreateFighter(AddFighterRequestDto newFighter);
         Task<ServiceResponse<GetFighterResponseDto>> UpdateFighter(UpdateFighterRequestDto updatedFighter);
